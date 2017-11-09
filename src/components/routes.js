f22fdaa19b7835616935bd08ab8232a63389546d
davquipe <davidcondori99@gmail.com>
2017-11-09 00:03:53 -0500

    first commit

---
+++ src/components/routes.js
@@ -0,0 +1,216 @@
+import { Content } from './app/AppContent'
+import { Home, Bus, Cart, About, Sandwiches }
+  from './app/AppContent'
+import CategoriaList from './categorias/List'
+import CategoriaForm from './categorias/Form'
+import ProductoList  from './productos/List'
+import ProductoForm  from './productos/Form'
+import CompraList from './compras/List'
+import CompraForm from './compras/Form'
+import VentaList  from './ventas/List'
+import VentaForm  from './ventas/Form'
+import DetalleVentaList  from './detalleVentas/List'
+import DetalleVentaForm  from './detalleVentas/Form'
+import DetalleCompraList  from './detalleCompras/List'
+import DetalleCompraForm  from './detalleCompras/Form'
+
+import Login from './Login'
+
+const routese = [
+  {
+    path: '/login',
+    title: 'Login!',
+    icon: 'home',
+    component: Login
+  }
+]
+////////////////////////////////////////////////////////////
+// then our route config
+const routes = [
+  {
+    path: '/home',
+    title: 'Home!',
+    icon: 'home',
+    exact: true,
+    component: Home
+  },
+
+
+  {
+    path: '/sandwiches',
+    title: 'sandwiches!',
+    icon: 'send',
+    component: Sandwiches
+  },
+  {
+    path: '/tacos',
+    title: 'tacos!',
+    icon: 'list',
+    component: Content,
+    routes: [
+      {
+        path: '/tacos/bus',
+        title: 'bus!',
+        icon: 'send',
+        component: Bus
+      },
+      {
+        path: '/tacos/cart',
+        component: Cart
+      },
+      {
+        path: '/tacos/about/:id',
+        title: 'About!',
+        icon: 'send',
+        component: About
+      }
+    ]
+  },
+  {
+    path: '/catalogo',
+    title: 'Catalogo!',
+    icon: 'list',
+    component: Content,
+    routes: [
+      {
+        path: '/catalogo/categorias/list',
+        exact: true,
+        title: 'Categorias!',
+        icon: 'send',
+        component: CategoriaList
+      },
+      {
+        path: '/catalogo/categorias/new',
+        exact: true,
+        title: 'Categoria New!',
+        icon: 'send',
+        component: CategoriaForm,
+        novisible: true
+      },
+      {
+        path: '/catalogo/categorias/edit/:id',
+        exact: true,
+        title: 'Categoria Edit!',
+        icon: 'send',
+        component: CategoriaForm,
+        novisible: true
+      },
+      {
+        path: '/catalogo/productos/list',
+        exact: true,
+        title: 'Productos!',
+        icon: 'send',
+        component: ProductoList
+      },
+      {
+        path: '/catalogo/productos/new',
+        exact: true,
+        title: 'Producto New!',
+        icon: 'send',
+        component: ProductoForm,
+        novisible: true
+      },
+      {
+        path: '/catalogo/productos/edit/:id',
+        exact: true,
+        title: 'Producto Edit!',
+        icon: 'send',
+        component: ProductoForm,
+        novisible: true
+      },
+      {
+        path: '/catalogo/ventas/list',
+        exact: true,
+        title: 'Ventas!',
+        icon: 'send',
+        component: VentaList
+      },
+      {
+        path: '/catalogo/ventas/new',
+        exact: true,
+        title: 'Venta New!',
+        icon: 'send',
+        component: VentaForm,
+        novisible: true
+      },
+      {
+        path: '/catalogo/ventas/edit/:id',
+        exact: true,
+        title: 'Venta Edit!',
+        icon: 'send',
+        component: VentaForm,
+        novisible: true
+      },
+      {
+        path: '/catalogo/detalleVentas/list',
+        exact: true,
+        title: 'DetalleVentas!',
+        icon: 'send',
+        component: DetalleVentaList
+      },
+      {
+        path: '/catalogo/detalleVentas/new',
+        exact: true,
+        title: 'DetalleVenta New!',
+        icon: 'send',
+        component: DetalleVentaForm,
+        novisible: true
+      },
+      {
+        path: '/catalogo/detalleVentas/edit/:id',
+        exact: true,
+        title: 'DetalleVenta Edit!',
+        icon: 'send',
+        component: DetalleVentaForm,
+        novisible: true
+      },
+      {
+        path: '/catalogo/compras/list',
+        exact: true,
+        title: 'Compras',
+        icon: 'shopping_cart',
+        component: CompraList
+      },
+      {
+        path: '/catalogo/compras/new',
+        exact: true,
+        title: 'Nueva Compras',
+        component: CompraForm,
+        novisible: true
+      },
+      {
+        path: '/catalogo/compras/edit/:id',
+        exact: true,
+        title: 'Editar Compra',
+        icon: 'shopping_cart',
+        component: CompraForm,
+        novisible: true
+      },
+      {
+        path: '/catalogo/detalleCompras/list',
+        exact: true,
+        title: 'Detalle Compras',
+        icon: 'dns',
+        component: DetalleCompraList
+      },
+      {
+        path: '/catalogo/detalleCompras/new',
+        exact: true,
+        title: 'Nueva Compras',
+        component: DetalleCompraForm,
+        novisible: true
+      },
+      {
+        path: '/catalogo/detalleCompras/edit/:id',
+        exact: true,
+        title: 'Editar Detalle Compra',
+        icon: 'dns',
+        component: DetalleCompraForm,
+        novisible: true
+      },
+      
+    ]
+  }
+]
+
+export { routes, routese }
---
+++ src/reducers/index.js
@@ -0,0 +1,29 @@
+import {combineReducers} from 'redux'
+import { authReducer as auth } from '../components/utils/OAuth2'
+
+//import counterReducer from './counterReducer'
+import categoria from './categoria-reducer'
+import producto from './producto-reducer'
+import compra from './compra-reducer'
+import venta from './venta-reducer'
+import detalleVenta from './detalleVenta-reducer'
+import detalleCompra from './detalleCompra-reducer'
+//import ecomm from './ecommReducer'
+import themeReducer from './appLayoutReducer'
+
+
+var reducers = combineReducers({
+  auth: auth,
+   // counter: counterReducer,
+  categoria: categoria,
+  producto: producto,
+  compra: compra,
+  venta: venta,
+  detalleVenta: detalleVenta,
+  detalleCompra: detalleCompra,
+  //  ecomm: ecomm,
+  theme:themeReducer,
+
+});
+
+export default reducers;
